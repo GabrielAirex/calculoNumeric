d4bb6a301dd19991f6ebe6a9b041bc192eb3add5
Gabriel Aires <gabrielafonsofreitas@gmail.com>
2021-11-09 21:49:07 -0300

    Adicionado erro relativo

--- secante.py
+++ secante.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 def f(x):
-    return x**(7)-x**(3)+0.1
+    return x**(2)*np.cos(x)+0.5
 
 
 
@@ -18,8 +18,8 @@ def secante(x0,x1, Erro, itMax):
         Er = np.abs((x-xa1)/x)
         it = it+1
     return (x, Er, it)
-x0=0.367
-x1=0.656
+x0=-2.68
+x1=-1.39
 Erro = 10**-4
 itMax=2
 res = secante(x0,x1, Erro, itMax)
